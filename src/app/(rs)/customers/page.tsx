9dc3ad56c1f24fecda34d463d9ddebdf496838e3
mariosknl <mariosknl@outlook.com>
2024-12-16 12:47:49 +0200

    customer table component created

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next";
 import CustomerSearch from "@/app/(rs)/customers/CustomerSearch";
 import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
+import * as Sentry from "@sentry/nextjs";
+import { CustomerTable } from "@/app/(rs)/customers/CustomerTable";
 
 export const metadata: Metadata = {
   title: "Customer Search",
@@ -15,12 +17,21 @@ export default async function Customers({
 
   if (!searchText) return <CustomerSearch />;
 
+  const span = Sentry.startInactiveSpan({
+    name: "getCustomerSearchResults-1",
+  });
+
   const results = await getCustomerSearchResults(searchText);
+  span.end();
 
   return (
     <>
       <CustomerSearch />
-      <p>{JSON.stringify(results)}</p>
+      {results.length ? (
+        <CustomerTable data={results} />
+      ) : (
+        <p className="mt-4">No results found</p>
+      )}
     </>
   );
 }
